75e1b992f9c68155848286c1cb5d8bf51eeeb34c
ant512 <devnull@localhost>
2011-06-14 16:14:25 +0100

    Purple block explodes.

---
+++ include/bmp/purpleblockexplodebmp.h
@@ -0,0 +1,11 @@
+#ifndef _PURPLEBLOCKEXPLODEBMP_H_
+#define _PURPLEBLOCKEXPLODEBMP_H_
+
+#include <bitmapwrapper.h>
+
+class PurpleBlockExplodeBmp : public WoopsiGfx::BitmapWrapper {
+public:
+	PurpleBlockExplodeBmp();
+};
+
+#endif
--- include/purpleblock.h
+++ include/purpleblock.h
@@ -10,6 +10,14 @@
 
 #include "purpleblockbmp.h"
 
+#include "purpleblockexplodebmp.h"
+
+#include "blockexplode1bmp.h"
+#include "blockexplode2bmp.h"
+#include "blockexplode3bmp.h"
+#include "blockexplode4bmp.h"
+#include "blockexplode5bmp.h"
+
 class PurpleBlock : public NormalBlock {
 public:
 	PurpleBlock() : NormalBlock(woopsiRGB(31, 0, 31)) {
@@ -29,9 +37,24 @@ public:
 		_bitmaps[CONNECTION_LEFT] = new PurpleBlockBmp();
 		_bitmaps[CONNECTION_RIGHT] = new PurpleBlockBmp();
 		_bitmaps[CONNECTION_LEFT | CONNECTION_RIGHT] = new PurpleBlockBmp();
+
+		_explodingAnim->addFrame(&_explodeBmp1, 0);
+		_explodingAnim->addFrame(&_explodeBmp2, 0);
+		_explodingAnim->addFrame(&_explodeBmp3, 0);
+		_explodingAnim->addFrame(&_explodeBmp4, 0);
+		_explodingAnim->addFrame(&_explodeBmp5, 0);
+		_explodingAnim->addFrame(&_explodeBmp6, 0);
 	};
 
 	~PurpleBlock() { };
+
+private:
+	PurpleBlockExplodeBmp _explodeBmp1;
+	BlockExplode1Bmp _explodeBmp2;
+	BlockExplode2Bmp _explodeBmp3;
+	BlockExplode3Bmp _explodeBmp4;
+	BlockExplode4Bmp _explodeBmp5;
+	BlockExplode5Bmp _explodeBmp6;
 };
 
 #endif
