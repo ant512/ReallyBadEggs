4c2f6f7a94d1b31f8abe8ef22da3e5d3aeb86231
ant512 <devnull@localhost>
2011-06-20 13:22:59 +0100

    Tweaked AI.

--- include/aicontroller.h
+++ include/aicontroller.h
@@ -52,7 +52,7 @@ public:
 			s32 columnY[Grid::GRID_WIDTH];
 
 			for (s32 i = 0; i < Grid::GRID_WIDTH; ++i) {
-				columnY[i] = (Grid::GRID_HEIGHT - grid->getColumnHeight(i));
+				columnY[i] = (Grid::GRID_HEIGHT - grid->getColumnHeight(i)) - 1;
 			}
 
 			s32 bestScore = 0;
@@ -64,12 +64,38 @@ public:
 			//   columns).
 			for (s32 i = 0; i <  Grid::GRID_WIDTH - 1; ++i) {
 
-				// Chain lengths
-				s32 chainLength = grid->getPotentialChainLength(i, columnY[i], grid->getBlockAt(liveBlock1.x, liveBlock1.y));
-				chainLength += grid->getPotentialChainLength(i, columnY[i], grid->getBlockAt(liveBlock2.x, liveBlock2.y));
+				s32 chainLength[2];
+				chainLength[0] = 0;
+				chainLength[1] = 0;
 
-				// Apply penalty for height of columns
-				s32 score = (chainLength * 20) / (columnY[i] + columnY[i + 1]);
+				// Don't try to add blocks to columns that are already as tall
+				// as the grid
+				if (columnY[i] >= 0) {
+					chainLength[0] = grid->getPotentialChainLength(i, columnY[i], grid->getBlockAt(liveBlock1.x, liveBlock1.y));
+				}
+
+				if (columnY[i + 1] >= 0) {
+					chainLength[1] = grid->getPotentialChainLength(i, columnY[i + 1], grid->getBlockAt(liveBlock2.x, liveBlock2.y));
+				}
+
+				// Basic score is the number of blocks connected together,
+				// regardless of whether explosions will be created
+				s32 chainBaseScore = (chainLength[0] + chainLength[1]) * 20;
+
+				// Extra score gives a bonus for creating exploding chains
+				s32 chainExtraScore = chainLength[0] > Grid::CHAIN_LENGTH ? 1 + chainLength[0] - Grid::CHAIN_LENGTH : 1;
+
+				// Join the chains together if the blocks are the same colour
+				if (grid->getBlockAt(liveBlock1.x, liveBlock1.y)->getColour() == grid->getBlockAt(liveBlock2.x, liveBlock2.y)->getColour()) {
+					chainExtraScore += chainLength[1];
+				} else {
+					chainExtraScore += chainLength[1] > Grid::CHAIN_LENGTH ? 1 + chainLength[1] - Grid::CHAIN_LENGTH : 1;
+				}
+
+				// Penalise for increasing the height of the target column
+				s32 heightPenalty = columnY[i] + columnY[i + 1];
+
+				s32 score = chainBaseScore * chainExtraScore / heightPenalty;
 
 				if (score > bestScore) {
 					bestScore = score;
