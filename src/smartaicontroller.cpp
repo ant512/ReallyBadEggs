c681f21b2148e1588760316f6f48172437b0a9d7
ant512 <devnull@localhost>
2011-06-22 15:33:32 +0100

    Removed duplicate code in smart AI class.

--- src/smartaicontroller.cpp
+++ src/smartaicontroller.cpp
@@ -32,54 +32,11 @@ void SmartAIController::analyseGrid() {
 		bool rotate = false;
 
 		// Get the score for each possible move we can make.  Assumptions:
-		// - We never rotate a block;
+		// - Blocks are only ever placed horizontally;
 		// - There is nothing to stop us moving a block (ie. no filled
 		//   columns).
-		for (s32 i = 0; i <  Grid::GRID_WIDTH - 1; ++i) {
+		for (s32 arrangement = 0; arrangement < 2; ++arrangement) {
 
-			s32 chainLength[2];
-			chainLength[0] = 0;
-			chainLength[1] = 0;
-
-			// Don't try to add blocks to columns that are already as tall
-			// as the grid
-			if (columnY[i] >= 0) {
-				chainLength[0] = grid->getPotentialChainLength(i, columnY[i], grid->getBlockAt(liveBlock1.x, liveBlock1.y));
-			}
-
-			if (columnY[i + 1] >= 0) {
-				chainLength[1] = grid->getPotentialChainLength(i, columnY[i + 1], grid->getBlockAt(liveBlock2.x, liveBlock2.y));
-			}
-
-			// Basic score is the number of blocks connected together,
-			// regardless of whether explosions will be created
-			s32 chainBaseScore = (chainLength[0] + chainLength[1]) * 20;
-
-			// Extra score gives a bonus for creating exploding chains
-			s32 chainExtraScore = chainLength[0] > Grid::CHAIN_LENGTH ? 1 + chainLength[0] - Grid::CHAIN_LENGTH : 1;
-
-			// Join the chains together if the blocks are the same colour
-			if (grid->getBlockAt(liveBlock1.x, liveBlock1.y)->getColour() == grid->getBlockAt(liveBlock2.x, liveBlock2.y)->getColour() &&
-				columnY[i] == columnY[i + 1]) {
-
-				chainExtraScore += chainLength[1];
-			} else {
-				chainExtraScore += chainLength[1] > Grid::CHAIN_LENGTH ? 1 + chainLength[1] - Grid::CHAIN_LENGTH : 1;
-			}
-
-			// Penalise for increasing the height of the target column
-			s32 heightPenalty = (Grid::GRID_HEIGHT * 2) - (columnY[i] + columnY[i + 1]);
-
-			s32 score = chainBaseScore * chainExtraScore / heightPenalty;
-
-			if (score > bestScore) {
-				bestScore = score;
-				bestScoreX = i;
-			}
-		}
-
-		// Try again, this time with rotation
-		if (grid->getBlockAt(liveBlock1.x, liveBlock1.y)->getColour() != grid->getBlockAt(liveBlock2.x, liveBlock2.y)->getColour()) {
 			for (s32 i = 0; i <  Grid::GRID_WIDTH - 1; ++i) {
 
 				s32 chainLength[2];
@@ -89,11 +46,11 @@ void SmartAIController::analyseGrid() {
 				// Don't try to add blocks to columns that are already as tall
 				// as the grid
 				if (columnY[i] >= 0) {
-					chainLength[0] = grid->getPotentialChainLength(i, columnY[i], grid->getBlockAt(liveBlock2.x, liveBlock2.y));
+					chainLength[0] = grid->getPotentialChainLength(i, columnY[i], grid->getBlockAt(liveBlock1.x, liveBlock1.y));
 				}
 
 				if (columnY[i + 1] >= 0) {
-					chainLength[1] = grid->getPotentialChainLength(i, columnY[i + 1], grid->getBlockAt(liveBlock1.x, liveBlock1.y));
+					chainLength[1] = grid->getPotentialChainLength(i, columnY[i + 1], grid->getBlockAt(liveBlock2.x, liveBlock2.y));
 				}
 
 				// Basic score is the number of blocks connected together,
@@ -120,9 +77,17 @@ void SmartAIController::analyseGrid() {
 				if (score > bestScore) {
 					bestScore = score;
 					bestScoreX = i;
-					rotate = true;
+
+					rotate = arrangement == 1;
 				}
 			}
+
+			// If both blocks are the same, we don't need to try the
+			// alternative rotation
+			if (grid->getBlockAt(liveBlock1.x, liveBlock1.y)->getColour() != grid->getBlockAt(liveBlock2.x, liveBlock2.y)->getColour()) break;
+			
+			// Switch the blocks and try again
+			grid->getLiveBlockPoints(liveBlock2, liveBlock1);
 		}
 
 		// Get a new target column
@@ -131,9 +96,9 @@ void SmartAIController::analyseGrid() {
 		if (rotate) _targetRotations = 2;
 	}
 
-	// Remember the co-ord of the block in the grid so that next time we
+	// Remember the co-ord of the top block in the grid so that next time we
 	// can identify if we've got a new live block or not
-	_lastLiveBlockY = liveBlock1.y;
+	_lastLiveBlockY = liveBlock1.y < liveBlock2.y ? liveBlock1.y : liveBlock2.y;
 }
 
 void SmartAIController::setGridRunner(const GridRunner* gridRunner) {
