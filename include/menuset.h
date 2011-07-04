678faee5457c80991ab59925dc08123da1cb8c4b
ant512 <devnull@localhost>
2011-07-04 13:38:44 +0100

    Added comments.

--- include/menuset.h
+++ include/menuset.h
@@ -10,8 +10,25 @@
 #include "menuoption.h"
 #include "soundplayer.h"
 
+/**
+ * A set of options that the player can choose from.  Options are arranged in a
+ * grid.
+ */
 class MenuSet {
 public:
+
+	/**
+	 * Constructor.
+	 * @param y The y co-ordinate of the menu set.  The x co-ordinate is
+	 * calculated automatically based on the size of the screen and the width of
+	 * the menu set such that the menu set is centred.
+	 * @param width The width of the menu set.
+	 * @param height The height of the menu set.
+	 * @param rows The number of rows in the menu set.
+	 * @param columns The number of columns in the menu set.
+	 * @param value The value of the menu set (a unique ID).
+	 * @param title The title of the menu set.
+	 */
 	MenuSet(s32 y, s32 width, s32 height, s32 rows, s32 columns, s32 value, const WoopsiGfx::WoopsiString& title) {
 		_y = y;
 		_width = width;
@@ -23,16 +40,30 @@ public:
 		_selectedOptionIndex = 0;
 	};
 
+	/**
+	 * Destructor.
+	 */
 	~MenuSet() {
 		for (s32 i = 0; i < _options.size(); ++i) {
 			delete _options[i];
 		}
 	};
 
+	/**
+	 * Adds a new options to the menu set.
+	 * @param text The text of the option.
+	 * @param value The value of the option.
+	 */
 	void addOption(const WoopsiGfx::WoopsiString& text, s32 value) {
 		_options.push_back(new MenuOption(text, value));
 	};
 
+	/**
+	 * Draws the menu set.
+	 * @param active True if the menu is active; false if not.
+	 * @param font The font to use to render text.
+	 * @param gfx The graphics object to draw to.
+	 */
 	void render(bool active, WoopsiGfx::FontBase* font, WoopsiGfx::Graphics* gfx) {
 
 		u16 titleColour = active ? woopsiRGB(31, 31, 31) : woopsiRGB(9, 9, 9);
@@ -70,6 +101,10 @@ public:
 		}
 	};
 
+	/**
+	 * Runs a single iteration of the menu set logic.  Reads the state of the
+	 * input controls to determine where to move the selection to.
+	 */
 	void iterate() {
 		const Pad& pad = Hardware::getPad();
 
@@ -88,6 +123,10 @@ public:
 		}
 	};
 
+	/**
+	 * Sets the selected option to the supplied index.
+	 * @param index The new selected index.
+	 */
 	void setSelectedOption(s32 index) {
 
 		s32 newValue = index;
@@ -104,20 +143,24 @@ public:
 		}
 	};
 
+	/**
+	 * Gets the value of the selected option.
+	 * @return The value of the selected option.
+	 */
 	s32 getSelectedValue() const {
 		return _options[_selectedOptionIndex]->getValue();
 	};
 
 private:
-	s32 _y;
-	s32 _width;
-	s32 _height;
-	s32 _selectedOptionIndex;
-	s32 _rows;
-	s32 _columns;
-	s32 _value;
-	WoopsiArray<MenuOption*> _options;
-	WoopsiGfx::WoopsiString _title;
+	s32 _y;								/**< The y co-ordinate of the menu set. */
+	s32 _width;							/**< The width of the menu set. */
+	s32 _height;						/**< The height of the menu set. */
+	s32 _selectedOptionIndex;			/**< The index of the selected option. */
+	s32 _rows;							/**< The number of rows in the menu set. */
+	s32 _columns;						/**< The number of columns in the menu set. */
+	s32 _value;							/**< The value of the menu set. */
+	WoopsiArray<MenuOption*> _options;	/**< The list of options in the menu set. */
+	WoopsiGfx::WoopsiString _title;		/**< The title of the menu set. */
 };
 
 #endif
