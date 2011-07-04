c42735a1bf8754ad2e087bd365e188f0a61d3492
ant512 <devnull@localhost>
2011-07-04 13:43:08 +0100

    Added comments.

--- include/menuoption.h
+++ include/menuoption.h
@@ -9,15 +9,38 @@
 #include "hardware.h"
 #include "pad.h"
 
+/**
+ * An option within a MenuSet object.
+ */
 class MenuOption {
 public:
+
+	/**
+	 * Constructor.
+	 * @param text The text of the option.
+	 * @param value The value of the option.
+	 */
 	MenuOption(const WoopsiGfx::WoopsiString& text, s32 value) {
 		_text = text;
 		_value = value;
 	};
 
+	/**
+	 * Destructor.
+	 */
 	~MenuOption() { };
 
+	/**
+	 * Draws the option.
+	 * @param x The x co-ordinate to draw to.
+	 * @param y The y co-ordinate to draw to.
+	 * @param width The width of the option.
+	 * @param height The height of the option.
+	 * @param selected True if the option is selected; false if not.
+	 * @param active True if the option is active; false if not.
+	 * @param font The font to draw text with.
+	 * @param gfx The Graphics object to draw with.
+	 */
 	void render(s32 x, s32 y, s32 width, s32 height, bool selected, bool active, WoopsiGfx::FontBase* font, WoopsiGfx::Graphics* gfx) {
 
 		u16 bgColour = selected ? (active ? woopsiRGB(31, 31, 0) : woopsiRGB(4, 4, 4)) : woopsiRGB(0, 0, 0);
@@ -32,13 +55,17 @@ public:
 		gfx->drawText(textX, textY, font, _text, 0, _text.getLength(), textColour);
 	};
 
+	/**
+	 * Gets the value of the option.
+	 * @return The value of the option.
+	 */
 	s32 getValue() const {
 		return _value;
 	};
 
 private:
-	WoopsiGfx::WoopsiString _text;
-	s32 _value;
+	WoopsiGfx::WoopsiString _text;		/**< The text of the option. */
+	s32 _value;							/**< The value of the option. */
 };
 
 #endif
